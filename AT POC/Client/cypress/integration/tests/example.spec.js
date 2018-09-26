36705e6e44ce8a62317dbba1b4ea5a73b03bd900
David McEwen <dlmcewen@uga.edu>
2018-09-26 14:05:56 -0400

    665

--- AT POC/Client/cypress/integration/tests/example.spec.js
+++ AT POC/Client/cypress/integration/tests/example.spec.js
@@ -2,7 +2,7 @@
 
 describe('Automated Test', function(){
 
-  it('Test', function() {
+  it('C478 Test', function() {
   Cypress.Screenshot.defaults({screenshotOnRunFailure: false})
 
     expect(true).to.equal(true)
